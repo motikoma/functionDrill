bec15fde3299b3127fc18aa8fe099d5af11b3dc6
motikoma <motikoma5125@gmail.com>
2020-11-14 18:44:50 +0900

    fix

--- src/slice.ts
+++
@@ -1,4 +0,0 @@
-import _ from "lodash";
-_.slice([1,2,3]);
-
-console.log([1,2,3].slice(1));
--- src/tail.ts
+++
@@ -1,6 +0,0 @@
-import _ from "lodash";
-_.tail([1, 2, 3]);
-// => [2, 3]
-
-const array = [1,2,3];
-console.log(array.slice(1));
